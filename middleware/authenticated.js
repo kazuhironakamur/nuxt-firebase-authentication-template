82fb19e18d8c2831bff0001ebe79db1315e03c88
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2020-07-02 15:55:04 +0900

    認証状態のstore名が間違えていたので修正

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,7 +1,7 @@
 export default function ({ route, store, redirect }) {
   // indexページへのアクセスは認証なしで許可する
   if (!(route.name === 'index')) {
-    const authenticated = store.getters['auth/get_authenticated']
+    const authenticated = store.getters['auth/getAuthenticated']
     if (!authenticated) {
       return redirect('/')
     }
